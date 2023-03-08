21440fa5189bab8b26704424a338e946886c6188
junkkie <jenkem029@gmail.com>
2023-03-08 10:37:55 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Detail from './components/Detail';
+import Navigation from './components/Navigation';
+import About from './routes/About';
+import Home from './routes/Home';
+import './styles/App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path='/' element={<Home />} /> {/* path='/'는 보통 홈화면을 의미 */}
+        <Route path='/about' element={<About />} />
+        <Route path='/detail' element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
